97ee98f21ec6d87eb0099965094bb0dbc60695ef
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-17 14:50:49 +0100

    primo push

---
+++ script.js
@@ -0,0 +1,36 @@
+let membri = [
+    {
+        nome: Walter, Gordon,
+        ruolo: OfficeManager,
+        foto: walter-gordon-office-manager.jpg,
+    },
+    {
+        nome: WayneBarnett,
+        ruolo: Founder&CEO,
+        foto: wayne-barnett-founder-ceo.jpg,
+    },
+    {
+        nome: AngelaCaroll,
+        ruolo: ChiefEditor,
+        foto: angela-caroll-chief-editor.jpg,
+    },
+    {
+        nome: AngelaLopez,
+        ruolo: SocialMediaManager,
+        foto: angela-lopez-social-media-manager.jpg,
+    },
+    {
+        nome: ScottEstrada,
+        ruolo: Developer,
+        foto: scott-estrada-developer.jpg,
+    },
+    {
+        nome: BarbaraRamos,
+        ruolo: GraphicDesigner,
+        foto: barbara-ramos-graphic-designer.jpg,
+    }
+]
+
+console.log(membri);
+
+
