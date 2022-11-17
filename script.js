01e110439f15906f733601f3af551a8bf7c300e2
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-17 16:05:37 +0100

    terzo push

--- script.js
+++ script.js
@@ -1,52 +1,65 @@
-//definire nomi, ruoli e foto----------------------------------------------------------
+//definizione nomi, ruoli e foto----------------------------------------------------------
 let membri = [
     {
-        nome: WalterGordon,
-        ruolo: OfficeManager,
-        foto: walter-gordon-office-manager.jpg,
+        nome: "WalterGordon",
+        ruolo: "OfficeManager",
+        foto: "walter-gordon-office-manager.jpg",
     },
     {
-        nome: WayneBarnett,
-        ruolo: Founder&CEO,
-        foto: wayne-barnett-founder-ceo.jpg,
+        nome: "WayneBarnett",
+        ruolo: "Founder&CEO",
+        foto: "wayne-barnett-founder-ceo.jpg",
     },
     {
-        nome: AngelaCaroll,
-        ruolo: ChiefEditor,
-        foto: angela-caroll-chief-editor.jpg,
+        nome: "AngelaCaroll",
+        ruolo: "ChiefEditor",
+        foto: "angela-caroll-chief-editor.jpg",
     },
     {
-        nome: AngelaLopez,
-        ruolo: SocialMediaManager,
-        foto: angela-lopez-social-media-manager.jpg,
+        nome: "AngelaLopez",
+        ruolo: "SocialMediaManager",
+        foto: "angela-lopez-social-media-manager.jpg",
     },
     {
-        nome: ScottEstrada,
-        ruolo: Developer,
-        foto: scott-estrada-developer.jpg,
+        nome: "ScottEstrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg2",
     },
     {
-        nome: BarbaraRamos,
-        ruolo: GraphicDesigner,
-        foto: barbara-ramos-graphic-designer.jpg,
+        nome: "BarbaraRamos",
+        ruolo: "GraphicDesigner",
+        foto: "barbara-ramos-graphic-designer.jpg",
     }
 ]
+
 //-------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 //----------------------------------------funzione---------------------------------------------
 
-function membriTeam(membri){
+function stampaMembriTeam(membri){
     console.log(
         `
-        ---------INFORMAZIONI----------
+        ------INFORMAZIONI MEMBRI------
         nome: ${membri.nome}
         ruolo: ${membri.ruolo}
         foto: ${membri.foto}
         -------------------------------
         `
-    )
+        )
+
 }
 //----------------------------------------------------------------------------------------------
